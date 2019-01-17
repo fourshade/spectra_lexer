b387da5917b0cc09b8165d80d3c58f4093fda93e
fourshade <synnethrelmn@yahoo.com>
2019-01-17 16:17:05 -0600

    Improve positioning of stroke separators in text graph

--- spectra_lexer/output/text/cascaded.py
+++ spectra_lexer/output/text/cascaded.py
@@ -1,7 +1,5 @@
 """ Module for generating a cascaded text graph. Nodes are drawn in an upper-triangular grid configuration. """
 
-from itertools import starmap
-
 from spectra_lexer.output.node import OutputNode
 from spectra_lexer.output.text.graph import TextGraph, TextGraphLine
 
@@ -16,45 +14,42 @@ class CascadedTextGraph(TextGraph):
     Aspect ratio is highly vertical, requiring an awkwardly shaped display window to accommodate.
     """
 
+    last_bounds = 9999
+
     def draw(self, node:OutputNode, offset:int=0, template:TextGraphLine=None) -> None:
         """ Add the body of a node, which includes its text line, connectors, and any descending children. """
         if template is None:
             template = TextGraphLine.filler(len(node.text))
         # Add the node text itself. If the last child is off the right end, add extensions to connect it.
-        self._copy_mutate_add(node, offset, template, TextGraphLine.add_node_string)
-        # If there are no children for this node, there is nothing else to do.
-        children = node.children
-        if children:
-            # Gather all node and offset data from children, not counting separators.
-            data = [(node, offset + node.attach_start) for node in children if not node.is_separator]
-            # Add all of the top containers at once.
+        n_text = TextGraphLine(template)
+        n_text.add_node_string(node, offset)
+        self.append(n_text)
+        # If there are children, prepare the top row and cascading connectors by building up starting from the right.
+        if node.children:
             top = TextGraphLine(template)
-            list(starmap(top.add_top_container, data))
+            lines = []
+            for child in reversed(node.children):
+                # Don't draw connectors for separators, but mark the previous child so it can draw the slashes.
+                if child.is_separator and lines:
+                    lines[-1][-1] = True
+                    continue
+                attach = offset + child.attach_start
+                lines.append([child, attach, template, False])
+                template = TextGraphLine(template)
+                top.add_top_container(child, attach)
+                template.add_connector(child, attach)
             self.append(top)
-            # Prepare the cascading connectors by building up starting from the right.
-            first = TextGraphLine(template)
-            lines = [_connector_add(template, *d) for d in reversed(data[1:])]
-            lines.reverse()
-            lines.append(first)
             # Add all normally-connected children (i.e. not separators) in order building down.
-            for i, ((node, offset), line) in enumerate(zip(data, lines)):
-                # if node.is_separator:
-                #     self[-1].add_separators()
-                #     continue
-                # Add the bottom container and start on the node body (and its children, if any) recursively.
-                if i > 0 or node.bottom_length > 1:
-                    # If the first child attaches with a single character, don't draw its bottom container line.
-                    self._copy_mutate_add(node, offset, line, TextGraphLine.add_bottom_container)
-                self.draw(node, offset, line)
-
-    def _copy_mutate_add(self, node:OutputNode, offset:int, line:TextGraphLine, meth:callable):
-        line = TextGraphLine(line)
-        meth(line, node, offset)
-        self.append(line)
-
-
-def _connector_add(line, node, offset):
-    line.add_connector(node, offset)
-    return TextGraphLine(line)
-
-# If a base node doesn't overlap the last line drawn, write the node text there instead of a new line.
+            for (node, offset, template, after_sep) in lines[-1::-1]:
+                # If the first child attaches with a single character, don't draw its bottom container line.
+                if node.attach_start > 0 or node.bottom_length > 1:
+                    # If a base node doesn't overlap the last line drawn, start drawing there instead of a new line.
+                    btemplate = TextGraphLine(template) if offset < self.last_bounds or after_sep else self.pop()
+                    # If this child follows a separator, the bottom container is a good place to divide the graph.
+                    if after_sep:
+                        btemplate.add_separators()
+                    btemplate.add_bottom_container(node, offset)
+                    self.append(btemplate)
+                    self.last_bounds = offset + node.bottom_length
+                # Start on the node body (and its children, if any) recursively.
+                self.draw(node, offset, template)
