79afd8199d59130c7d637992e3759a2e2531f004
fourshade <synnethrelmn@yahoo.com>
2019-02-16 13:21:35 -0600

    Process GUI events manually

--- spectra_lexer/gui_qt/base.py
+++ spectra_lexer/gui_qt/base.py
@@ -1,6 +1,10 @@
 """ Base module for the GUI Qt package. """
 
-from spectra_lexer.core.base import Subprocess
+from typing import Any
+
+from PyQt5.QtWidgets import QApplication
+
+from spectra_lexer import Subprocess
 from spectra_lexer.gui_qt.board import GUIQtBoardDisplay
 from spectra_lexer.gui_qt.config import GUIQtConfig
 from spectra_lexer.gui_qt.console import GUIQtConsoleDisplay
@@ -25,10 +29,18 @@ class GUIQt(Subprocess):
 
     ROLE = "gui"
 
-    window: MainWindow  # Main window must be publicly accessible for the Plover plugin.
+    window: MainWindow        # Main window; must be publicly accessible for the Plover plugin.
+    process_events: callable  # Callback to handle GUI events after every engine call.
 
     def __init__(self):
         """ Create the main window and assemble all child components with their required widgets. """
         self.window = MainWindow()
         cmp_args = self.window.partition()
         super().__init__(*COMPONENTS, args_iter=[cmp_args[w] for w in COMPONENTS.values()])
+        self.process_events = QApplication.instance().processEvents
+
+    def call(self, *args, **kwargs) -> Any:
+        """ Manually process events after every engine call to avoid hanging. """
+        value = super().call(*args, **kwargs)
+        self.process_events()
+        return value
