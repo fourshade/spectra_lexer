8922914cc425e922ac295d76e6720fc875e5a165
fourshade <synnethrelmn@yahoo.com>
2019-04-10 00:53:37 -0500

    Reformat rules and add fingerspelling to board diagram

--- spectra_lexer/steno/rules.py
+++ spectra_lexer/steno/rules.py
@@ -10,6 +10,7 @@ class RuleFlags:
     STROKE = "STRK"    # Exact match for a single stroke, not part of one. Handled by exact dict lookup.
     WORD = "WORD"      # Exact match for a single word. These rules do not adversely affect lexer performance.
     RARE = "RARE"      # Rule applies to very few words and could specifically cause false positives.
+    OPTIONAL = "OPT"   # Optional or redundant rule. May be informational; removal will cause little effect.
     INVERSION = "INV"  # Inversion of steno order. Child rule keys will be out of order with respect to the parent.
     UNMATCHED = "BAD"  # Incomplete lexer result. This rule contains all the unmatched keys and no letters.
     GENERATED = "GEN"  # Lexer generated rule. This is always the root unless there are special circumstances.
