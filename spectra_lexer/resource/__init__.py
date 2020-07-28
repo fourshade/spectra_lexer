dfeada427fee43e1070f0168c7f45ae4615f84f5
fourshade <synnethrelmn@yahoo.com>
2020-07-28 18:41:49 -0500

    Add hashing to frozen struct

--- spectra_lexer/resource/__init__.py
+++ spectra_lexer/resource/__init__.py
@@ -7,5 +7,14 @@ from typing import NoReturn
 class FrozenStruct(SimpleNamespace):
     """ Immutable attribute-based data structure. """
 
-    def __setattr__(self, *args) -> NoReturn:
+    def _raise_on_mutate(self, *args) -> NoReturn:
         raise AttributeError('Structure is immutable.')
+
+    __setattr__ = __delattr__ = _raise_on_mutate
+
+    # Identity-based hashing and equality are cheap and usually sufficient.
+    # We *could* do value-based hashing on the contents of __dict__ since the structure is frozen,
+    # but this is ungodly expensive and makes dictionary lookups over 20 times slower.
+    __hash__ = object.__hash__
+    __eq__ = object.__eq__
+    __ne__ = object.__ne__
